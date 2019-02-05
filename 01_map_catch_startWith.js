698a122aaafa8864c7c1096398002208f5510a53
Sergey Koshechkin <serge.koshechkin@gmail.com>
2019-02-05 16:34:18 +0300

    update 01_map_catch_startWith rxjs 6

--- 01_map_catch_startWith.js
+++ 01_map_catch_startWith.js
@@ -1,24 +1,21 @@
+const { of, throwError } = Rx;
+const { map, take, delay, materialize, dematerialize, catchError } = RxOperators;
+
 /**
  * Демо с https://rxviz.com/
  */
 
 // Возвращает загруженные данные через 800мс
-const load = (id) => Rx.Observable
-	.of(id)
-	.delay(800)
+const load = (id) => of(id).pipe(delay(800));
 
 // Возвращает ошибку через 400мс
-const loadWithError = (id) => Rx.Observable
-	.throw(`Error id: ${id}`)
-	.materialize()
-	.delay(400)
-	.dematerialize()
+const loadWithError = (id) => throwError(`Error id: ${id}`).pipe(delay(400));
 
 
-const loadRandom = (id) => Math.random() > 0.5 ?
-	load(id) : loadWithError(id)
+const loadRandom = (id) => Math.random() > 0.5 ? load(id) : loadWithError(id)
 
-loadRandom(1)
-	.map((id) => `LOADED id:${id}`)
-	.catch(() => Rx.Observable.of('ERROR'))
-//	.startWith('PENDING')
+loadRandom(1).pipe(
+  map((id) => `LOADED id:${id}`),
+  catchError(() => of('ERROR')),
+  //startWith('PENDING'),
+)
