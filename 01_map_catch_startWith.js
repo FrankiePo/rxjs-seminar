c7f7a9ac40eebbb226f2ef250c92a2b6d5368ec4
Sergey Koshechkin <serge.koshechkin@gmail.com>
2019-02-06 16:51:45 +0300

    Update 01_map_catch_startWith.js

--- 01_map_catch_startWith.js
+++ 01_map_catch_startWith.js
@@ -1,5 +1,5 @@
-const { of, throwError } = Rx;
-const { map, take, delay, materialize, dematerialize, catchError } = RxOperators;
+const { of, throwError, timer } = Rx;
+const { map, take, delay, materialize, dematerialize, catchError, switchMapTo, startWith } = RxOperators;
 
 /**
  * Демо с https://rxviz.com/
@@ -9,7 +9,7 @@ const { map, take, delay, materialize, dematerialize, catchError } = RxOperators
 const load = (id) => of(id).pipe(delay(800));
 
 // Возвращает ошибку через 400мс
-const loadWithError = (id) => throwError(`Error id: ${id}`).pipe(delay(400));
+const loadWithError = (id) => timer(400).pipe(switchMapTo(throwError(`Error id: ${id}`)));
 
 
 const loadRandom = (id) => Math.random() > 0.5 ? load(id) : loadWithError(id)
