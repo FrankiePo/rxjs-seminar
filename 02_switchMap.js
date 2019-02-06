cd8e426443e1bd5fff6ba70e328a9409cd9443b3
Sergey Koshechkin <serge.koshechkin@gmail.com>
2019-02-06 17:02:16 +0300

    Update 02_switchMap.js

--- 02_switchMap.js
+++ 02_switchMap.js
@@ -1,20 +1,20 @@
-// Возвращает данные пациента через 800мс
-const loadPatient = (patientId) => Rx.Observable
-	.of({
-		id: patientId,
-		tasks: ['id1', 'id2', 'id3'],
-	})
-	.delay(800)
+const { of } = Rx;
+const { take, delay, map, switchAll, switchMap } = RxOperators;
+
+// Возвращает данные клиента через 800мс
+const loadPatient = (clientId) => of({
+  id: clientId,
+  tasks: ['id1', 'id2', 'id3'],
+}).pipe(delay(800));
 
 
-// Возвращает задачи пациента через 400 секунд
+// Возвращает задачи клиента через 400 секунд
 // [1,2,3] =*> ['Task 1', 'Task 2', 'Task 3']
-const loadTaskByIds = (ids) => Rx.Observable
-	.of(ids.map((id) => `Task ${id}`))
-	.delay(400)
+const loadTaskByIds = (ids) => of(ids.map((id) => `Task ${id}`)).pipe(delay(400));
 
+loadPatient(10).pipe(
+//	map((client) => loadTaskByIds(client.tasks)),
+//	switchAll(),
+	switchMap((client) => loadTaskByIds(client.tasks)),
+);
 
-loadPatient(10)
-//	.map((patient) => loadTaskByIds(patient.tasks))
-//	.switch()
-//	.switchMap((patient) => loadTaskByIds(patient.tasks))
