ac9cd5c8065a13a16c1d2f4698c3307ec4f79959
Sergey Koshechkin <serge.koshechkin@gmail.com>
2019-02-06 17:04:05 +0300

    Update 02_switchMap.js

--- 02_switchMap.js
+++ 02_switchMap.js
@@ -2,7 +2,7 @@ const { of } = Rx;
 const { take, delay, map, switchAll, switchMap } = RxOperators;
 
 // Возвращает данные клиента через 800мс
-const loadPatient = (clientId) => of({
+const loadClient = (clientId) => of({
   id: clientId,
   tasks: ['id1', 'id2', 'id3'],
 }).pipe(delay(800));
@@ -12,7 +12,7 @@ const loadPatient = (clientId) => of({
 // [1,2,3] =*> ['Task 1', 'Task 2', 'Task 3']
 const loadTaskByIds = (ids) => of(ids.map((id) => `Task ${id}`)).pipe(delay(400));
 
-loadPatient(10).pipe(
+loadClient(10).pipe(
 //	map((client) => loadTaskByIds(client.tasks)),
 //	switchAll(),
 	switchMap((client) => loadTaskByIds(client.tasks)),
